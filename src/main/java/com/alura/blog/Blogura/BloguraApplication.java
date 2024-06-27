6899c2204149bdf400c3a9685a0488fa5a6041a5
Diego Romero <dilero@gmail.com>
2024-06-26 21:04:53 -0500

    first commit

---
+++ src/main/java/com/alura/blog/Blogura/BloguraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.blog.Blogura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloguraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/blog/Blogura/BloguraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.blog.Blogura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloguraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
